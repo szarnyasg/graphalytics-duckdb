07c85057752be15cca58d2114d81f060e314cc5f
Gabor Szarnyas <szarnyasg@gmail.com>
2021-09-27 16:50:33 +0200

    Add directed LCC implementation

--- gx.py
+++ gx.py
@@ -29,15 +29,35 @@ if undirected:
 print("====================")
 print("LCC")
 print("====================")
+con.execute("""CREATE VIEW neighbors AS (
+    SELECT e.source AS vertex, e.target AS neighbor
+    FROM e
+    UNION
+    SELECT e.target AS vertex, e.source AS neighbor
+    FROM e
+    )
+""")
 con.execute("""
-    SELECT e1.source, CAST(count(*) AS float) / (count(DISTINCT e2.target)*(count(DISTINCT e2.target)-1))
-    FROM e e1, e e2, e e3
-    WHERE e1.target = e2.source
-      AND e2.target = e3.source
-      AND e3.target = e1.source
-    GROUP BY e1.source
-    ORDER BY e1.source ASC
-    """)
+SELECT
+  id,
+  CASE WHEN tri = 0 THEN 0.0 ELSE (CAST(tri AS float) / (deg*(deg-1))) END AS value
+FROM (
+    SELECT
+    v.id AS id,
+    (SELECT count(*) FROM neighbors WHERE neighbors.vertex = v.id) AS deg,
+    (SELECT count(*)
+    FROM neighbors n1
+    JOIN neighbors n2
+      ON n1.vertex = n2.vertex
+    JOIN e e3
+      ON e3.source = n1.neighbor
+     AND e3.target = n2.neighbor
+    WHERE n1.vertex = v.id
+    ) AS tri
+    FROM v
+    ORDER BY v.id ASC
+) s
+""")
 results = con.fetchall()
 for result in results:
     print(result)
